8291ca344dd4f87797d3169494926847c33e60b8
abdosakoury78 <abdosakoury78@gmail.com>
2026-04-03 00:13:05 +0200

    make shop page

--- src/app/Pages/shop/shop.component.ts
+++ src/app/Pages/shop/shop.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FilterComponent } from '../../Components/filter/filter.component';
+import { Product } from '../../Model/product';
+import { ProductsService } from '../../Services/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
-  imports: [FilterComponent],
+  imports: [FilterComponent, CommonModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private productsService : ProductsService) {}
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        console.error("Error fetching products:", err);
+      }
+    })
+  }
+
+  trackById(index: number, product: Product): number {
+    return product.id;
+  }
 }
